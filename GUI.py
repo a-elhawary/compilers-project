27e88472bc163bce9471a538a84a97769e37bcbc
Abdelrahman Elhawary <abdelrahmanelhawary@Abdelrahmans-MacBook-Pro.local>
2022-04-21 02:29:10 +0200

    finished mvp phase 1

--- GUI.py
+++ GUI.py
@@ -2,7 +2,11 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QHBoxLayout
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtSvg import QSvgWidget
+from engine import engine
 import sys
 
 WIDTH = 1080
@@ -22,18 +26,102 @@ window.setWindowTitle('PyQt5 App')
 window.setGeometry(500, 100, WIDTH , HEIGHT)
 
 windowLayout = QVBoxLayout()
+windowLayout.setContentsMargins(0,0,0,0)
 
-#Top Bar
+#Top Bar Section
 topBar = QWidget()
 topBar.setStyleSheet("""QWidget{
     background-color:#448e96;
+    padding:15px;
 }
 """)
 topBarLayout = QHBoxLayout()
-topBarLayout.addWidget(QLabel("Compilers Project"))
+topBarLayout.setContentsMargins(0,0,0,0)
+topBarLayout.addWidget(QLabel("Compilers Project"), 2)
+expressionInput = QLineEdit()
+expressionInput.setStyleSheet(""" 
+    background-color:#fff;
+    margin:20px;
+    border-radius:8px;
+""")
+left = QWidget()
+leftLayout = QVBoxLayout()
+leftLayout.addStretch()
+temp = QWidget()
+def leftLayoutFunc(tokens, isValid):
+    for i in reversed(range(leftLayout.count())):
+        widget = leftLayout.itemAt(i).widget()
+        if widget is not None:
+            widget.setParent(None)
+    leftLayout.setContentsMargins(0,0,0,0)
+    if isValid:
+        validLabel = QLabel("Valid Expression!")
+        validLabel.setStyleSheet("""
+            color:green;
+        """)
+    else:
+        validLabel = QLabel("Invalid Expression!")
+        validLabel.setStyleSheet("""
+            color:red;
+        """)
+    leftLayout.insertWidget(0,validLabel)
+    for i in reversed(range(len(tokens))):
+        token = tokens[i]
+        widget = QLabel("<" + token[0] + ", " + token[1] + ">" + " Transition: " + str(token[2]) + " -> " + str(token[3]))
+        leftLayout.insertWidget(0,widget)
+    left.update()
+
+def onStart():
+    isValid, tokens = engine(expressionInput.text())
+    leftLayoutFunc(tokens, isValid)
+    renderImage()
+    tokens.clear()
+
+main = QWidget()
+image = QSvgWidget()
+def renderImage():
+    image.load("./dfa.svg")
+    main.update()
+
+sendExpressionButton = QPushButton("Start")
+sendExpressionButton.clicked.connect(onStart)
+sendExpressionButton.setStyleSheet("""
+    background-color:#212121;
+    color:#fff;
+    margin-right:15px;
+    border-radius:8px;
+""")
+topBarLayout.addWidget(expressionInput, 2)
+topBarLayout.addWidget(sendExpressionButton)
 topBar.setLayout(topBarLayout)
 windowLayout.addWidget(topBar)
-windowLayout.addWidget(QLabel("Main"), 3)
+
+# Main Section
+mainLayout = QHBoxLayout()
+mainLayout.setContentsMargins(0,0,0,0)
+
+#Left Section
+left.setStyleSheet("""
+    background-color:#dedede;
+""")
+left.setLayout(leftLayout)
+
+mainLayout.addWidget(left, 1)
+#Right Section
+dfaContainer = QWidget()
+dfaLayout = QVBoxLayout()
+regexContainer = QWidget()
+regexLayout = QHBoxLayout()
+regexLayout.addStretch()
+regexLayout.addWidget(QLabel("((ID | NUM) OP)* (ID | NUM)"))
+regexContainer.setLayout(regexLayout)
+dfaLayout.addWidget(regexContainer)
+dfaLayout.addWidget(image, 1)
+dfaContainer.setLayout(dfaLayout)
+mainLayout.addWidget(dfaContainer, 3)
+main.setLayout(mainLayout)
+
+windowLayout.addWidget(main, 3)
 
 window.setLayout(windowLayout)
 
--- engine.py
+++ engine.py
@@ -7,9 +7,6 @@ def isNumber(character):
 def isOperator(character):
     return character == "+" or character == "-" or character == "*" or character == "/"
 
-def addToken(token, tokenType):
-    print(token + " " + tokenType)
-
 def tokenizer(character, currToken):
     newState = 0
     if isOperator(character):
@@ -26,99 +23,111 @@ def tokenizer(character, currToken):
         newState = 2
     return newState, currToken
 
-# get input
-expression = input("Please enter an Expression: ")
+tokens = []
 
-isValid = True
-currentState = 1
-tempCurrentState = 1
-currentToken = ""
-previousState = None
+def addToken(token, tokenType, currentState, nextState, isValid):
+    if(isValid):
+        tokens.append([token , tokenType, currentState, nextState])
+    else:
+        tokens.append([token , tokenType, 6, 6])
 
-# loop over expression character by character
-for character in expression:
-    if currentState == 1:
-        if character == "-":
-            currentToken += character
-            tempCurrentState = 2
-        elif isNumber(character):
-            currentToken += character
-            tempCurrentState = 3
-        elif isAlphabet(character):
-            currentToken += character
-            tempCurrentState = 4
-        elif character != " ":
-            tempCurrentState, currentToken = tokenizer(character, currentToken)
-            isValid = False
-    elif currentState == 2:
-        if isNumber(character):
-            currentToken += character
-            tempCurrentState = 3
-        elif character != " ":
-            tempCurrentState, currentToken = tokenizer(character, currentToken)
-            isValid = False
-    elif currentState == 3:
-        if isOperator(character):
-            addToken(currentToken, "Number")
-            addToken(character, "Operator")
-            currentToken = ""
-            tempCurrentState = 5
-        elif isNumber(character):
-            currentToken += character
-            tempCurrentState = 3
-        elif character == " ":
-            addToken(currentToken, "Number")
-            currentToken = ""
-            tempCurrentState = 7
-        else:
-            tempCurrentState, currentToken = tokenizer(character, currentToken)
-            isValid = False
+def engine(expression):
+    isValid = True
+    currentState = 1
+    tempCurrentState = 1
+    currentToken = ""
+    previousState = None
+
+    # loop over expression character by character
+    for character in expression:
+        if currentState == 1:
+            if character == "-":
+                currentToken += character
+                tempCurrentState = 2
+            elif isNumber(character):
+                currentToken += character
+                tempCurrentState = 3
+            elif isAlphabet(character):
+                currentToken += character
+                tempCurrentState = 4
+            elif character != " ":
+                tempCurrentState, currentToken = tokenizer(character, currentToken)
+                isValid = False
+        elif currentState == 2:
+            if isNumber(character):
+                currentToken += character
+                tempCurrentState = 3
+            elif character != " ":
+                tempCurrentState, currentToken = tokenizer(character, currentToken)
+                isValid = False
+        elif currentState == 3:
+            if isOperator(character):
+                addToken(currentToken, "Number", currentState, 5, isValid)
+                addToken(character, "Operator", currentState, 5, isValid)
+                currentToken = ""
+                tempCurrentState = 5
+            elif isNumber(character):
+                currentToken += character
+                tempCurrentState = 3
+            elif character == " ":
+                addToken(currentToken, "Number", currentState, 7, isValid)
+                currentToken = ""
+                tempCurrentState = 7
+            else:
+                tempCurrentState, currentToken = tokenizer(character, currentToken)
+                isValid = False
+        elif currentState == 4:
+            if isOperator(character):
+                addToken(currentToken, "ID", currentState, 5, isValid)
+                addToken(character, "Operator", currentState, 5, isValid)
+                currentToken = ""
+                tempCurrentState = 5
+            elif isNumber(character) or isAlphabet(character):
+                currentToken += character
+                tempCurrentState = 4
+            elif character == " ":
+                addToken(currentToken, "ID", currentState, 7, isValid)
+                currentToken = ""
+                tempCurrentState = 7
+            else:
+                tempCurrentState, currentToken = tokenizer(character, currentToken)
+                isValid = False
+        elif currentState == 5:
+            if isNumber(character):
+                currentToken += character
+                tempCurrentState = 3
+            elif character == "-":
+                currentToken += character
+                tempCurrentState = 2
+            elif isAlphabet(character):
+                currentToken += character
+                tempCurrentState = 4
+            elif character != " ":
+                addToken(character, "Operator", currentStatel, 5, isValid)
+                currentState = 5
+                isValid = False
+        elif currentState == 7:
+            if isOperator(character):
+                addToken(character, "Operator", currentState, 5, isValid)
+                tempCurrentState = 5
+            elif character != " ":
+                tempCurrentState, currentToken = tokenizer(character, currentToken)
+                isValid = False
+        previousState = currentState
+        currentState = tempCurrentState
+
+    if(currentState == 3):
+        addToken(currentToken, "Number", currentState, currentState, isValid)
     elif currentState == 4:
-        if isOperator(character):
-            addToken(currentToken, "ID")
-            addToken(character, "Operator")
-            currentToken = ""
-            tempCurrentState = 5
-        elif isNumber(character) or isAlphabet(character):
-            currentToken += character
-            tempCurrentState = 4
-        elif character == " ":
-            addToken(currentToken, "ID")
-            currentToken = ""
-            tempCurrentState = 7
-        else:
-            tempCurrentState, currentToken = tokenizer(character, currentToken)
-            isValid = False
-    elif currentState == 5:
-        if isNumber(character):
-            currentToken += character
-            tempCurrentState = 3
-        elif character == "-":
-            currentToken += character
-            tempCurrentState = 2
-        elif isAlphabet(character):
-            currentToken += character
-            tempCurrentState = 4
-        elif character != " ":
-            addToken(character, "Operator")
-            currentState = 5
-            isValid = False
-    elif currentState == 7:
-        if isOperator(character):
-            addToken(character, "Operator")
-            tempCurrentState = 5
-        elif character != " ":
-            tempCurrentState, currentToken = tokenizer(character, currentToken)
-            isValid = False
-    previousState = currentState
-    currentState = tempCurrentState
+        addToken(currentToken, "ID", currentState, currentState, isValid)
 
-if(currentState == 3):
-    addToken(currentToken, "Number")
-elif currentState == 4:
-    addToken(currentToken, "ID")
+    return isValid and (currentState == 4 or currentState == 3), tokens
 
-if isValid and (currentState == 4 or currentState == 3):
-    print("Valid Expression")
-else:
-    print("Invalid Expression")
+def main():
+    expression = input("enter ay haga: ")
+    isValid, tokens = engine(expression)
+    print(tokens)
+    print(isValid)
+
+if __name__ == "__main__":
+    main()
